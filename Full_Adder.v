d12fc9c04ab8fa3654fbec2e9bce7dcf6def4016
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2024-04-21 22:19:45 +0530

    Create Full_Adder.v

---
+++ Full_Adder.v
@@ -0,0 +1,9 @@
+// Structural Model
+
+module fulladdstr(sum,carry,a,b,c);
+outputsum,carry;
+inputa,b,c;
+xor g1(sum,a,b,c);
+and g2(x,a,b); and g3(y,b,c); and g4(z,c,a);
+or g5(carry,x,z,y);
+endmodule
