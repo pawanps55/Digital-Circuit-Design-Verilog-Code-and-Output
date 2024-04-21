cea3f97d43910b0aa95d59b752d993284653b514
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2024-04-21 22:24:08 +0530

    Create FULL SUBTRACTOR.v

---
+++ FULL SUBTRACTOR.v
@@ -0,0 +1,13 @@
+// Structural Model
+
+module fullsubtstr(diff,borrow,a,b,c); 
+output diff,borrow; 
+input a,b,c;
+wire a0,q,r,s,t; 
+not(a0,a); 
+xor(x,a,b);
+xor(diff,x,c); 
+and(y,a0,b); 
+and(z,~x,c); 
+or(borrow,y,z); 
+endmodule
